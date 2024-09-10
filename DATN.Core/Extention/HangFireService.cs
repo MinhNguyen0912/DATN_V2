1b4206bf5a28427dadef1a4889d095aa7e5d79bf
PDucViet <116140911+PDucViet@users.noreply.github.com>
2024-09-10 20:47:41 +0700

    cxxx

---
+++ DATN.Core/Extention/HangFireService.cs
@@ -0,0 +1,20 @@
+﻿using Hangfire;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DATN.Core.Extention
+{
+    public static class HangFireService
+    {
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHangfire(config =>
+                config.UseSqlServerStorage("DATNDbContextConnection")); // Cấu hình kết nối SQL Server
+            services.AddHangfireServer();
+        }
+    }
+}
