cda9c618025e7207bde38b0e9fdf45a2fab5966f
phucnk <140143143+Hoangzero8688@users.noreply.github.com>
2024-09-16 08:44:27 +0700

    create 5

---
+++ DATN.Core/ViewModel/AttributeEAVVM/CreateAttributeEAVVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DATN.Core.ViewModel.AttributeEAVVM
+{
+    public class CreateAttributeEAVVM
+    {
+        public string Name { get; set; }
+    }
+}
--- DATN.Core/ViewModel/AttributeVM/AtttributeVariation/CreateMultipleValue.cs
+++
@@ -1,9 +0,0 @@
-﻿using DATN.Core.ViewModel.AttributeVM.AttributeDynamic;
-
-namespace DATN.Core.ViewModel.AttributeVM.AtttributeVariation
-{
-    public class CreateMultipleValue
-    {
-        public List<CreateVLVariantVM> listValue { get; set; }
-    }
-}
