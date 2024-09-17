d8f88b9bd277be2852139c92f12daf456b861dd2
vannha18 <vannha180102@gmail.com>
2024-09-17 16:06:46 +0700

    aa

---
+++ DATN.Core/ViewModel/Paging/AttributesPaging.cs
@@ -0,0 +1,14 @@
+﻿using DATN.Core.ViewModel.Product_EAV;
+using DATN.Core.ViewModels.Paging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DATN.Core.ViewModel.Paging
+{
+    public class AttributesPaging : PagingRequestBase<AttributeVM_EAV>
+    {
+    }
+}
